81566cd0cc3792c302a86ebf07335f0008793296
Денис Пономарев <deskin196@gmail.com>
2019-11-12 15:03:38 +0300

    Написал проверку ввода часов времени прихода и изменил поля ввода даты рабочего дня и времени прихода

--- Calendar/PropertiesMenu.cs
+++ Calendar/PropertiesMenu.cs
@@ -9,19 +9,93 @@ namespace Calendar
 {
     public class PropertiesMenu: INotifyPropertyChanged
     {
-        public String _WorkDay = Properties.Settings.Default.WorkDay.ToString("dd'.'MM'.'yyyy");
-        public String WorkDay
+        public String _WorkDayDD = Properties.Settings.Default.WorkDay.ToString("dd");
+        public String WorkDayDD
         {
             get 
             { 
-                return _WorkDay; 
+                return _WorkDayDD; 
             }
             set 
             {
-                _WorkDay = value;
-                OnPropertyChanged("WorkDay");
+                _WorkDayDD = value;
+                OnPropertyChanged("WorkDayDD");
             }
         }
+        public String _WorkDayMM = Properties.Settings.Default.WorkDay.ToString("MM");
+        public String WorkDayMM
+        {
+            get 
+            { 
+                return _WorkDayMM; 
+            }
+            set 
+            {
+                _WorkDayMM = value;
+                OnPropertyChanged("WorkDayMM");
+            }
+        }
+        public String _WorkDayYYYY = Properties.Settings.Default.WorkDay.ToString("yyyy");
+        public String WorkDayYYYY
+        {
+            get 
+            { 
+                return _WorkDayYYYY; 
+            }
+            set 
+            {
+                _WorkDayYYYY = value;
+                OnPropertyChanged("WorkDayYYYY");
+            }
+        }
+        public String _TimeToArrivalHH = Properties.Settings.Default.TimeToArrival.ToString("hh");
+        public String TimeToArrivalHH
+        {
+            get
+            {
+                return _TimeToArrivalHH;
+            }
+            set
+            {
+                int result;
+                //Если передано число
+                if (int.TryParse(value, out result) || value == "")
+                {
+                    if (value != "")
+                    {
+                        if (result >= 0)
+                        {
+                            //Проверяем, меньше ли оно 23
+                            if (Convert.ToInt32(value) <= 23)
+                            {
+                                _TimeToArrivalHH = value;
+                                OnPropertyChanged("TimeToArrivalHH");
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        _TimeToArrivalHH = value;
+                        OnPropertyChanged("TimeToArrivalHH");
+                    }
+
+                }
+            }
+        }
+        public String _TimeToArrivalMM = Properties.Settings.Default.TimeToArrival.ToString("mm");
+        public String TimeToArrivalMM
+        {
+            get
+            {
+                return _TimeToArrivalMM;
+            }
+            set
+            {
+                _TimeToArrivalMM = value;
+                OnPropertyChanged("TimeToArrivalMM");
+            }
+        }
+
         public PropertiesMenu() 
         { 
         
