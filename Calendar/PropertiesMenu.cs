31fbfca798c0826d00a5341f06a4f67851aab9d3
Денис Пономарев <deskin196@gmail.com>
2019-11-21 18:43:43 +0300

    Изменил меню настроек

--- Calendar/PropertiesMenu.cs
+++ Calendar/PropertiesMenu.cs
@@ -9,6 +9,19 @@ namespace Calendar
 {
     public class PropertiesMenu: INotifyPropertyChanged
     {
+        public String _RandomInterval = Properties.Settings.Default.MaxTimeSpread.Minutes.ToString();
+        public String RandomInterval
+        {
+            get 
+            { 
+                return _RandomInterval; 
+            }
+            set 
+            {
+                _RandomInterval = value;
+                OnPropertyChanged("RandomInterval");
+            }
+        }
         public String _WorkDayDD = Properties.Settings.Default.WorkDay.ToString("dd");
         public String WorkDayDD
         {
@@ -111,6 +124,74 @@ namespace Calendar
                     {
                         _TimeToArrivalMM = value;
                         OnPropertyChanged("TimeToArrivalMM");
+                    }
+
+                }
+            }
+        }
+        public String _TimeToLiavingHH = Properties.Settings.Default.LiavingTime.ToString("hh");
+        public String TimeToLiavingHH
+        {
+            get
+            {
+                return _TimeToLiavingHH;
+            }
+            set
+            {
+                int result;
+                //Если передано число
+                if (int.TryParse(value, out result) || value == "")
+                {
+                    if (value != "")
+                    {
+                        if (result >= 0)
+                        {
+                            //Проверяем, меньше ли оно 23
+                            if (Convert.ToInt32(value) <= 23)
+                            {
+                                _TimeToLiavingHH = value;
+                                OnPropertyChanged("TimeToLiavingHH");
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        _TimeToLiavingHH = value;
+                        OnPropertyChanged("TimeToLiavingHH");
+                    }
+
+                }
+            }
+        }
+        public String _TimeToLiavingMM = Properties.Settings.Default.LiavingTime.ToString("mm");
+        public String TimeToLiavingMM
+        {
+            get
+            {
+                return _TimeToLiavingMM;
+            }
+            set
+            {
+                int result;
+                //Если передано число
+                if (int.TryParse(value, out result) || value == "")
+                {
+                    if (value != "")
+                    {
+                        if (result >= 0)
+                        {
+                            //Проверяем, меньше ли оно 23
+                            if (Convert.ToInt32(value) <= 59)
+                            {
+                                _TimeToLiavingMM = value;
+                                OnPropertyChanged("TimeToLiavingMM");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _TimeToLiavingMM = value;
+                        OnPropertyChanged("TimeToLiavingMM");
                     }
 
                 }
